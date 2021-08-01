2bf204666c61e88145368246a30494c158079906
Davee Sok <daveesok@gmail.com>
2021-07-31 19:00:37 -0500

    fixed categorylist

--- components/CategoryList.js
+++ components/CategoryList.js
@@ -1,13 +1,40 @@
 import React from 'react';
+import Button from '@material-ui/core/Button';
+import ButtonGroup from '@material-ui/core/ButtonGroup';
+import { makeStyles } from '@material-ui/core/styles';
 
-function CategoryList({ categories }) {
-  console.log('🚀 ~ categories line 4', categories);
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    '& > *': {
+      margin: theme.spacing(1),
+    },
+    display: 'none',
+    [theme.breakpoints.up('sm')]: {
+      display: 'flex',
+    },
+    borderBottom: '1px solid #e0e0e0',
+  },
+  button: {
+    textTransform: 'none',
+  },
+}));
+
+export default function CategoryList({ categories }) {
+  const classes = useStyles();
+  console.log('categories: ', categories);
 
   return (
-    <div>
-      <h1>CategoryList</h1>
+    <div className={classes.root}>
+      <ButtonGroup variant="text" color="inherit" fullWidth>
+        {categories.map((obj) => (
+          <Button className={classes.button} key={obj.id}>
+            {obj.name}
+          </Button>
+        ))}
+      </ButtonGroup>
     </div>
   );
 }
-
-export default CategoryList;
